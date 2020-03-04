c0eb78111d721391d242fbcfc00a727134698d40
Chris Thorpe <55049521+cjthorpe@users.noreply.github.com>
2020-03-04 13:13:26 +0000

    Update tags.tf

--- terraform-aws-codebuild-module/tags.tf
+++ terraform-aws-codebuild-module/tags.tf
@@ -2,6 +2,6 @@ module "tags" {
   AZ              = var.AZ 
   environment     = var.environment 
   project_service = var.project_service 
-  source          = "git@github.com:cjthorpe/terraform-aws-modules//co-terraform-module-tags" 
+  source          = "git@github.com:cjthorpe/terraform-aws-modules//terraform-aws-tags-module" 
 }
 
